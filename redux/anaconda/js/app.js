d851457fbaba3231e661c433faca0c02f86f7761
marccharton <kramoogle@gmail.com>
2019-11-18 00:49:01 +0100

    feat: more conventional counter

--- redux/anaconda/js/app.js
+++ redux/anaconda/js/app.js
@@ -1,30 +1,43 @@
 import { anaconda } from "./anaconda.js";
 import { createStore } from 'redux';
 
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+function increment() {
+    return { type : INCREMENT };
+}
+
+function decrement() {
+    return { type : DECREMENT };
+}
+
+
 function counter(state = 0, action) {
     switch(action.type) {
-        case "INCREMENT":
+        case INCREMENT:
             return state + 1;
-        case "DECREMENT":
+        case DECREMENT:
             return state - 1;
         default:
             return state;
     }
 }
 
-let store = createStore(counter);
+const store = createStore(counter);
+
 store.subscribe(() => {
     console.log(store.getState());
 });
 
 document.getElementById("increment").addEventListener("click", () => {
-    store.dispatch({ type: "INCREMENT" });
+    store.dispatch(increment());
 });
 
 document.getElementById("decrement").addEventListener("click", () => {
-    store.dispatch({ type: "DECREMENT" });
+    store.dispatch(decrement());
 });
 
-store.dispatch({ type: "INCREMENT" });
-store.dispatch({ type: "DECREMENT" });
-store.dispatch({ type: "DECREMENT" });
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
