1c45a312196adc6af0ff6a5c83881dd7020bc294
marccharton <kramoogle@gmail.com>
2019-11-18 00:35:28 +0100

    feat: basic counter

--- redux/anaconda/js/app.js
+++ redux/anaconda/js/app.js
@@ -1,4 +1,22 @@
 import { anaconda } from "./anaconda.js";
 import { createStore } from 'redux';
 
-anaconda();
+function counter(state = 0, action) {
+    switch(action.type) {
+        case "INCREMENT":
+            return state + 1;
+        case "DECREMENT":
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+let store = createStore(counter);
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
+store.dispatch({ type: "DECREMENT" });
